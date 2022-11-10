14443ff9830bd253ad191fc78b1e27a24087e9d6
HamadNoor <nhamad919@gmail.com>
2022-11-10 10:12:15 +0400

    class activity

---
+++ Class activity/functions.py
@@ -0,0 +1,13 @@
+def sum():
+    a = int(input("First number: "))
+    b = int(input("second number: "))
+    sum = a + b
+    print(sum)
+    
+    
+def subtract(a,b):
+    sum = a - b
+    print(sum)
+subtract(5)
+
+ 
---
+++ Class activity/percentage_function.py
@@ -0,0 +1,13 @@
+def average():
+    a = int(input("first number: "))
+    b = int(input("second number: "))
+    c = int(input("third number: "))
+    d = int(input("fourth number: "))
+    sum = a + b + c + d
+    average = sum/4
+    print(average)
+average()
+
+
+        
+    
