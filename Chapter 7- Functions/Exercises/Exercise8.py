e48f52cc3509b0570148657804839630aad02573
HamadNoor <nhamad919@gmail.com>
2022-11-15 20:02:22 +0400

    Update Exercise8.py

--- Chapter 7- Functions/Exercises/Exercise8.py
+++ Chapter 7- Functions/Exercises/Exercise8.py
@@ -1,10 +1,9 @@
 #Creating a funtion called add with one argument(num)
 def add(num):
-# If the value for num is given
     if num:
-# Return the value and add the value for num subtract 1
+# Return this result
         return num + add(num - 1)
-# If the value for num is not given then return 0
+# If the value for num is 0, then return 0
     else:
         return 0
 # Assigning a variable(prod) to the function with the argument for num as 10
